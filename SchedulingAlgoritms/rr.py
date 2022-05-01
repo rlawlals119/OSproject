4e031190cf516bcd32c784fade80e331bf0bf5bb
rlawlals119 <75199007+rlawlals119@users.noreply.github.com>
2022-05-01 20:10:55 +0900

    Add files via upload
    Round Robin 알고리즘 코드 수정본 업로드

--- SchedulingAlgoritms/rr.py
+++ SchedulingAlgoritms/rr.py
@@ -7,26 +7,25 @@ from queue import Queue
 PROCCESSES = int(input("프로세스 수를 입력하시오. : ")) # 사용되는 프로세스 수 입력
 PROCCESSORS = 1
 TIME = 0        # 프로그램 전체 수행 시간
-T_QUANTUM = 2
+T_QUANTUM = 0
 max_time = 20    # 모든 BT를 합한 값, 모든 프로세스의 총 수행 시간
 bt = 0
 at = 0      # 입력받은 BT, AT 값
 
-#BT = list(np.zeros(PROCCESSES+1))                   # (0위치 비움)0으로 채워진 프로세스 수만큼의 크기를 가진 리스트
+T_QUANTUM = int(input("time quantum을 입력하시오 : "))
+BT = list(np.zeros(PROCCESSES+1))                   # (0위치 비움)0으로 채워진 프로세스 수만큼의 크기를 가진 리스트
+for i in range(0, PROCCESSES):
+    bt = int(input("P{0}의 BT : ".format(i+1)))     # bt 입력
+    BT[i+1] = bt                                    # 인덱스를 프로세스 번호라고 생각하고 각 프로세스의 bt값 넣음
+    max_time += bt                                  # 모든 프로세스의 총 수행시간
+AT = list(np.zeros(max_time))                       # 0으로 채워진 총 수행시간 크기의 리스트
 
-#for i in range(0, PROCCESSES):
-#    bt = int(input("P{0}의 BT : ".format(i+1)))     # bt 입력
-#    BT[i+1] = bt                                    # 인덱스를 프로세스 번호라고 생각하고 각 프로세스의 bt값 넣음
-#    max_time += bt                                  # 모든 프로세스의 총 수행시간
+for i in range(0, PROCCESSES):
+    at = int(input("P{0}의 AT : ".format(i+1)))     # at 입력
+    AT[at] = i+1                                    # 인덱스는 도착한 시간 넣어진 값은 프로세스의 번호
 
-#AT = list(np.zeros(max_time))                       # 0으로 채워진 총 수행시간 크기의 리스트
-#
-#for i in range(0, PROCCESSES):
-#    at = int(input("P{0}의 AT : ".format(i+1)))     # at 입력
-#    AT[at] = i+1                                    # 인덱스는 도착한 시간 넣어진 값은 프로세스의 번호
-
-AT = [1, 2, 0, 3, 0, 4, 5]
-BT = [0, 3, 7, 2, 5, 3]
+#AT = [1, 2, 0, 3, 0, 4, 5]
+#BT = [0, 3, 7, 2, 5, 3]
 
 print(AT)
 print(BT)
@@ -38,22 +37,36 @@ rr_t = T_QUANTUM
 while (TIME < max_time):
     if(TIME <= 6 and AT[TIME] != 0) : queue.append(AT[TIME])
     TIME += 1
-    if current_p == 0 or rr_t == 0:          # 프로세서 할당 가능
+    if current_p == 0 or rr_t == T_QUANTUM:          # 프로세서 할당 가능
         current_p = queue.pop(0) # 현재 프로세서에 queue 원소 하나 넣음
-        rr_t = T_QUANTUM
         print("proccess : ", current_p)        # 현재 프로세서 넘버 출력
 
-    if current_p != 0 and rr_t != 0:
-        BT[current_p] -= 1      # 현재 프로세스의 남은 수행 시간
+    if current_p != 0:          # 프로세서 할당된 상태
+        BT[current_p] -= 1
         rr_t -= 1
-        if rr_t == 0:
-            current_p = 0
-            queue.append(current_p)
-        elif BT[current_p] == 0 : # 현재 프로세스 수행 완료 시
+
+        if BT[current_p] == 0:  # 수행완료된 경우
             print("P{0}의 TT : {1}".format(current_p, TIME))    # 프로세스 실행 시간
-            current_p = 0       # 현재 프로세스 비워줌
-            rr_t = 0
-        else : queue.append(current_p)
+            current_p = 0           # 현재 프로세스 비워줌
+            rr_t = T_QUANTUM
+
+        elif rr_t == 0:         # 사용 제한 시간을 모두 수행한 경우
+            queue.append(current_p)
+            current_p = 0
+            rr_t = T_QUANTUM
+
+
+    #if current_p != 0 and rr_t != 0:
+    #    BT[current_p] -= 1      # 현재 프로세스의 남은 수행 시간
+    #    rr_t -= 1
+    #    if rr_t == 0:
+    #        current_p = 0
+    #        queue.append(current_p)
+    #    elif BT[current_p] == 0 : # 현재 프로세스 수행 완료 시
+    #        print("P{0}의 TT : {1}".format(current_p, TIME))    # 프로세스 실행 시간
+    #        current_p = 0       # 현재 프로세스 비워줌
+    #        rr_t = 0
+    #    queue.append(current_p)
 
 end = time()
 
