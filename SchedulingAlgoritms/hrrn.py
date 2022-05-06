ab843bfb36c501c5f53593e552874b5c26eeda5d
meniscusrock <103157980+meniscusrock@users.noreply.github.com>
2022-05-06 18:27:20 +0900

    Add files via upload
    hrrn function

--- SchedulingAlgoritms/hrrn.py
+++ SchedulingAlgoritms/hrrn.py
@@ -1,97 +1,129 @@
-def sortByArrival(at, n):
-     
-    # Selection Sort applied 
-    for i in range(0, n - 1):
-        for j in range(i + 1, n):
-             
-            # Check for lesser arrival time 
-            if at[i] > at[j]:
-                 
-                # Swap earlier process to front
-                at[i], at[j] = at[j], at[i]
- 
+'''
 # 코드 시작
 if __name__ == '__main__':
+    PROCESSE = 5 # 프로세스 개수 설정
+    W = 0        # 소비전력
      
-    max_time = 0  # Burst Time 합계
-    avgWT = 0   # Waiting Time 평균
-    avgTT = 0   # Turnaround Time 평균
-    n = 5       # 프로세스 개수 설정
-     
-    completed =         [0] * n     # 완료 개수
-    WT =      [0] * n     # WT
-    TT =   [0] * n     # TT
-    NTT =     [0] * n     # NTT
+    Done =  [0] * (PROCESSE+1)     # 완료 개수
+    WT =    [0] * (PROCESSE+1)     # Waiting time
+    TT =    [0] * (PROCESSE+1)     # Turnaround time
+    NTT =   [0] * (PROCESSE+1)     # Normalized TT
     
-    arrival_time =  [0, 1, 3, 5, 6]    # Arrival Times 설정   
-    burst_time =    [3, 7, 2, 5, 3]    # Burst Times 설정
-    process =       []              # 프로세스
+    AT = [0, 0, 1, 3, 5, 6]    # Arrival Times 설정   
+    BT = [0, 3, 7, 2, 5, 3]    # Burst Times 설정
+    PROCESSES = [0]          # 프로세스
+
+    sum_BT = 0   # Burst Time 합계
+    avg_WT = 0   # Waiting Time 평균
+    avg_TT = 0   # Turn-around Time 평균
      
     # 프로세스 이름 지정 (A, B, C, D)
-    for i in range(0, n):           # 프로세스 개수만큼 생성
-        process.append(chr(65 + i)) # 프로세스 이름 삽입 (A, B, C, D)
-        max_time += burst_time[i]     # BT 합계
+    for i in range(1, PROCESSE + 1):    # 프로세스 개수만큼 생성
+        PROCESSES.append(chr(64 + i))      # 프로세스 이름 삽입 (A, B, C, D)
+        sum_BT += BT[i]             # BT 합계
+         
+    # Arriaval Time 대로 정렬한다.
+    for i in range(1, PROCESSE):        # i: 0부터 3까지
+        for j in range(i + 2, PROCESSE + 1):    # j: 1부터 4까지 ex) (0,1), (0,2)
+            if AT[i] > AT[j]:               # AT의 대소비교
+                AT[i], AT[j] = AT[j], AT[i] # AT가 빠른 프로세스와 바꾼다.
+    
+    
+    #for i in range(0, 5):
+    #    print(ID[i] )
+    
+
+    print("#### WT = TT - BT ####", "\t\t", "#### NTT = TT/BT ####")
+    print("PROCESS", "\t", "AT", "\t\t",
+          "BT", "\t\t", "WT", "\t\t",
+          "TT", "\t\t", "NTT")
+
+    ##########################################################################
+    ############################## 스케줄링 시작 ##############################
+    ##########################################################################
+
+    TIME = AT[1]     # 첫 프로세스 도착 시간 and 수행시간(초)
+
+    # BT 합계까지 TIME을 증가시킨다.
+    while(TIME < sum_BT):
+        # 응답률 설정 (Response Ratio = (WT + BT) / BT))
+        HRR = 0     # High Response Ratio
+        NRR = 0     # Now Response Ratio
+        index = 0   # Index 위치
         
-    sortByArrival(arrival_time, n)  # Arriaval Time 대로 정렬한다.
-    print("Name            ", "AT             ",
-          "BT             ", "WT             ",
-          "T              ", "TT              ")
-    t = arrival_time[0]     # 첫 프로세스 도착 시간
-     
-    while(t < max_time):
-         
-        # 응답률의 하한 설정 (Response Ratio = (WT + BT) / BT))
-        hrr = -9999
-        temp, loc = 0, 0
-         
-        for i in range(0, n):
-             
+        for i in range(1, PROCESSE+1):
+            
             # 프로세스가 도착했을 때를 확인한다.
+            # 도착 시간이 현재보다 빠르고, 스케줄링이 완료되지 않았다면.
             # 완료를 판별 0: 처리 이전, 1: 처리 완료
-            if arrival_time[i] <= t and completed[i] != 1:
+            if AT[i] <= TIME and Done[i] == 0:
                  
-                # Response Ratio 계산 (Response Ratio = (WT + BT) / BT))
-                temp = ((burst_time[i] +
-                        (t - arrival_time[i])) /
-                         burst_time[i])
+                # Now Response Ratio 계산 (Response Ratio = (WT + BT) / BT))
+                NRR = (((TIME - AT[i]) + BT[i]) / BT[i])
                           
-                # 가장 높은 Response Ratio 확인
-                if hrr < temp:
-                     
-                    # Response Ratio 저장 
-                    hrr = temp
-                     
-                    # 프로세스 인덱스 위치
-                    loc = i
+                if HRR < NRR:  # 가장 높은 Response Ratio 확인
+                    HRR = NRR  # Response Ratio 저장 
+                    index = i  # 프로세스 인덱스 위치
                     
-        # Updating time value 
-        t += burst_time[loc]
+        # Highest Response Ratio을 가진 프로세스를 처리
+        TIME += BT[index]                   # 총 실행시간
+        TT[index] = TIME - AT[index]        # Turn-Around Time 계산
+        WT[index] = (TT[index] - BT[index]) # Waiting Time 계산
+        NTT = (TT[index] / BT[index])       # Normalized TT 계산
+
+        avg_TT += TT[index]   # Turn-Around Time 평균 계산
+        avg_WT += WT[index]   # Waiting Time 평균 계산
         
-        # Waiting Time 계산
-        WT[loc] = (t - arrival_time[loc] -
-                                 burst_time[loc])
-         
-        # Turn-Around Time 계산
-        TT[loc] = t - arrival_time[loc]
-         
-        # 평균을 위한 Turn-Around Time 합 계산
-        avgTT += TT[loc]
-         
-        # Normalized TT 계산
-        NTT = float(TT[loc] /
-                              burst_time[loc])
-         
-        # 완료된 프로세스를 처리한다.
-        completed[loc] = 1
-         
-        # 평균을 위한 Waiting Time 합 계산
-        avgWT += WT[loc]
-         
-        print(process[loc], "\t\t", arrival_time[loc],
-              "\t\t", burst_time[loc], "\t\t",
-              WT[loc], "\t\t",
-              TT[loc], "\t\t",
-              "{0:.6f}".format(NTT))
- 
-    print("Average Waiting Time: {0:.6f}".format(avgWT / n))
-    print("Average Turn-Around Time:  {0:.6f}".format(avgTT / n))
+        Done[index] = 1       # 완료된 프로세스를 처리
+        
+        # 스케줄링이 끝나는 순서대로 출력
+        print(PROCESSES[index], "\t\t", AT[index], "\t\t",
+              BT[index], "\t\t", WT[index], "\t\t",
+              TT[index], "\t\t",
+              "{0:.2f}".format(NTT))
+        
+    print("WT 평균: {0:.2f}".format(avg_WT / PROCESSE))
+    print("TT 평균: {0:.2f}".format(avg_TT / PROCESSE))
+    print(TIME) 
+'''
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import heapq
+import numpy as np
+import copy
+
+def hrrn (PROCESSES, TIME, AT, BT, at):
+    Done =  list(np.zeros(PROCESSES + 1))     # 완료 개수
+    WT =    list(np.zeros(PROCESSES + 1))     # Waiting time
+    TT =    list(np.zeros(PROCESSES + 1))     # Turnaround time
+    NTT =   list(np.zeros(PROCESSES + 1))     # Normalized TT
+    PROCESSES = [0]                         # 프로세스
+    sum_BT = 0   # Burst Time 합계
+    for i in range(1, PROCESSES + 1):       # 프로세스 개수만큼 생성
+        #PROCESSES.append(chr(64 + i))      # 프로세스 이름 삽입 (A, B, C, D)
+        sum_BT += BT[i]                     # BT 합계
+    for i in range(1, PROCESSES + 1):           # AT 정렬
+        for j in range(i + 2, PROCESSES + 1):    # j: 1부터 5까지 ex) (0,1), (0,2)
+            if AT[i] > AT[j]:               # AT의 대소비교
+                AT[i], AT[j] = AT[j], AT[i] # AT가 빠른 프로세스와 바꾼다.
+    while(TIME < sum_BT):
+        HRR = 0     # High Response Ratio
+        NRR = 0     # Now Response Ratio
+        index = 0   # Index 위치
+        for i in range(1, PROCESSES + 1):
+            if AT[i] <= TIME and Done[i] == 0:
+                NRR = (((TIME - AT[i]) + BT[i]) / BT[i])
+                if HRR < NRR:  # 가장 높은 Response Ratio 확인
+                    HRR = NRR  # Response Ratio 저장 
+                    index = i  # 프로세스 인덱스 위치
+        TIME += BT[index]                   # 총 실행시간
+        TT[index] = TIME - AT[index]        # Turn-Around Time 계산
+        WT[index] = (TT[index] - BT[index]) # Waiting Time 계산
+        NTT = (TT[index] / BT[index])       # Normalized TT 계산
+        Done[index] = 1       # 완료된 프로세스를 처리
+    print("P id| AT | BT | WT | TT")
+    for i in range (1, PROCESSES+1):
+        print("P{0} | {1} | {2} | {3} | {4}".format(i, AT[i], BT[i], WT[i], TT[i]))
+
+    return WT, TT
