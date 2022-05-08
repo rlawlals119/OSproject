2c564781dd32ba52237834797ee910ce5dbdf8e1
rlawlals119 <75199007+rlawlals119@users.noreply.github.com>
2022-05-08 18:18:25 +0900

    Add files via upload
    while(TIME > 20)  ----> 오류
    max_time 추가로 오류 해결

--- SchedulingAlgoritms/ui_test.py
+++ SchedulingAlgoritms/ui_test.py
@@ -23,6 +23,7 @@ class WindowClass(QMainWindow, form_class):
         self.TT = []
         self.WT = []
         self.name = 0
+        self.max_time = 0
         self.setupUi(self)
 
         self.start_pushButton.clicked.connect(self.startBtn)
@@ -31,6 +32,7 @@ class WindowClass(QMainWindow, form_class):
 
     def startBtn(self):
         self.PROCESSES = self.name
+        self.max_time = sum(self.BT)
         current_Algorithm = str(self.AlgorithomList_comboBox.currentText())
         at_tmp = self.AT.copy()
         for i in range(1, self.at+1):
@@ -84,17 +86,24 @@ class WindowClass(QMainWindow, form_class):
         self.TimeTable_tableWidget.clear()
         self.ResultTable_tableWidget.clear()
 
+    def GanttChart(self, TIME, current_p):
+        self.Ganttchart_tableWidget.setColumnCount(TIME)
+        self.Ganttchart_tableWidget.setRowCount(1)
+        self.Ganttchart_tableWidget.setItem(0, TIME, str(current_p))
+
+
     def fcfs_ui(self, PROCESSES, TIME, AT, BT, at):
         queue = []
         current_p = 0                   # 현재 프로세스
         tmp = BT.copy()
         TT = list(np.zeros(PROCESSES+1))
         WT = list(np.zeros(PROCESSES+1))
-        while (TIME < 20):
+        while (TIME < self.max_time):
             if(TIME <= at and AT[TIME] != 0) : queue.append(tmp[AT[TIME]])
             TIME += 1
             if current_p == 0:          # 프로세서 할당 가능
                 current_p = tmp.index(queue.pop(0)) # 현재 프로세서에 queue 원소 하나 넣음
+                #self.GanttChart(TIME, current_p)
                 print("proccess : ", current_p)        # 현재 프로세서 넘버 출력
             if current_p != 0:
                 tmp[current_p] -= 1      # 현재 프로세스의 남은 수행 시간
@@ -113,7 +122,7 @@ class WindowClass(QMainWindow, form_class):
         tmp = BT.copy()
         rr_t = T_QUANTUM
         if(AT[0] != 0) : queue.append(AT[TIME])
-        while (TIME < 20):
+        while (TIME < self.max_time):
             TIME += 1
             if(TIME <= at and AT[TIME] != 0) : queue.append(AT[TIME])
             if current_p == 0 or rr_t == T_QUANTUM:          # 프로세서 할당 가능
@@ -146,7 +155,7 @@ class WindowClass(QMainWindow, form_class):
         WT = list(np.zeros(PROCESSES+1))
         print(AT)
         print(BT)
-        while (TIME < 20):
+        while (TIME < self.max_time):
             if(TIME <= at and AT[TIME] != 0) : heapq.heappush(heap, tmp[AT[TIME]])
             TIME += 1
             if current_p == 0:          # 프로세서 할당 가능
@@ -169,7 +178,7 @@ class WindowClass(QMainWindow, form_class):
         tmp = BT.copy()     
         TT = list(np.zeros(PROCESSES+1))
         WT = list(np.zeros(PROCESSES+1))           
-        while (TIME < 20):
+        while (TIME < self.max_time):
             if(TIME <= at and AT[TIME] != 0) : heapq.heappush(heap, tmp[AT[TIME]])
             TIME += 1
             current_p = tmp.index(heapq.heappop(heap)) # 현재 프로세서에 heap에서 가장 작은 원소 넣음
